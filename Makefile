ec2ff2bcf5b1e47dd8d82f333dbd4e51bb5a03f0
Kirill Emelyanov <ekirill@ekirill.ru>
2021-01-31 18:15:46 +0300

    added rich text editor

--- Makefile
+++ Makefile
@@ -10,6 +10,9 @@ up: migrate
 shell:
 	docker-compose -f ./docker-compose.yaml -f ./docker-compose-dev.yaml exec app bash
 
+makemigrations: build
+	docker-compose -f ./docker-compose.yaml -f ./docker-compose-dev.yaml run --rm app python /app/manage.py makemigrations
+
 down:
 	docker-compose -f ./docker-compose.yaml -f ./docker-compose-dev.yaml down
 
--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,6 @@
+from ckeditor.widgets import CKEditorWidget
 from django.contrib import admin
-from django.forms import BaseInlineFormSet
+from django import forms
 
 from news.models import NewsEntry, SiteSection
 
@@ -8,7 +9,7 @@ class SiteSectionAdmin(admin.ModelAdmin):
     ordering = ('title',)
 
 
-class SiteSectionInlineFormSet(BaseInlineFormSet):
+class SiteSectionInlineFormSet(forms.BaseInlineFormSet):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.select_related('site_section')
@@ -22,7 +23,17 @@ class SiteSectionInline(admin.TabularInline):
     fields = ('site_section', 'custom_short_text')
 
 
+class NewsEntryForm(forms.ModelForm):
+    short_text = forms.CharField(widget=CKEditorWidget(), label='Короткое описание')
+    long_text = forms.CharField(widget=CKEditorWidget(), label='Полный текст (не обязательно)', required=False)
+
+    class Meta:
+        model = NewsEntry
+        fields = ('published_at', 'title', 'short_text', 'long_text')
+
+
 class NewsEntryAdmin(admin.ModelAdmin):
+    form = NewsEntryForm
     ordering = ('-published_at',)
 
     inlines = [

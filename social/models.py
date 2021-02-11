cd11d672b91209d51f7cf0fb97b53a6cbf2805fa
Kirill Emelyanov <ekirill@ekirill.ru>
2021-02-11 19:18:09 +0300

    comment db model

---
+++ social/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Comment(models.Model):
+    entry = models.ForeignKey('news.NewsEntry', verbose_name='Документ', null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey('core.StroiUser', verbose_name='Автор', null=True, on_delete=models.SET_NULL)
+    published_at = models.DateTimeField('Дата публикации', default=now)
+    is_visible = models.BooleanField('Виден всем', default=True)
+    reply_to = models.ForeignKey('social.Comment', verbose_name='Ответ на', null=True, on_delete=models.SET_NULL)
+    message = models.CharField('Сообщение', max_length=2000)
